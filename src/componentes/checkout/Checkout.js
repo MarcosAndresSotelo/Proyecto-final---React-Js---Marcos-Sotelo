90454bf93fb6e74bd16df6ca240e78ab8c560042
MarcosSotelo <marcosandresotelo@gmail.com>
2023-02-04 20:56:40 -0300

    terminando proyecto

--- src/componentes/checkout/Checkout.js
+++ src/componentes/checkout/Checkout.js
@@ -9,13 +9,13 @@ import "../checkout/checkout.scss"
 import Swal from "sweetalert2"
 
 const schema = Yup.object().shape({
-    nombre: Yup.string().min(4, 'Mínimo 4 caracteres').max(30, 'Máximo 30 caracteres').required('Este campo es requerido'),
-    apellido: Yup.string().min(4, 'Mínimo 4 caracteres').max(30, 'Máximo 30 caracteres').required('Este campo es requerido'),
-    telefono: Yup.string().required('Este campo es requerido'),
-    correo: Yup.string().email('El email no es válido').required('Este campo es obligatorio'),
-    repetirCorreo: Yup.string().when("correo", {
+    nombre: Yup.string().min(4, 'Mínimo 4 caracteres').max(30, 'Máximo 30 caracteres').required('Este campo es obligatorio'),
+    apellido: Yup.string().min(4, 'Mínimo 4 caracteres').max(30, 'Máximo 30 caracteres').required('Este campo es obligatorio'),
+    telefono: Yup.number().required('Este campo es obligatorio'),
+    email: Yup.string().email('El email no es válido').required('Este campo es obligatorio'),
+    repetirEmail: Yup.string().when("email", {
         is: val => !!(val && val.length > 0),
-        then: Yup.string().oneOf([Yup.ref("correo"), null], "Los correos electrónicos no coinciden").required("Este campo es obligatorio")
+        then: Yup.string().oneOf([Yup.ref("email"), null], "Los emails no coinciden").required("Este campo es obligatorio")
     })
 })
 
@@ -75,7 +75,7 @@ export const Checkout = () => {
         return (
             <div className="container my-5 text-center">
                 <h2 className="fs-2 fw-bolde">Tu compra ha sido exitosa</h2>
-                <Link to="/inicio" className="btn btn-secondary mt-5">Volver</Link>
+                <Link to="/" className="btn btn-secondary mt-5">Volver</Link>
             </div>
         )
     }
@@ -92,7 +92,8 @@ export const Checkout = () => {
             <Formik
                 initialValues={{
                     nombre: '',
-                    direccion: '',
+                    apellido: '',
+                    telefono: '',
                     email: ''
                 }}
                 onSubmit={(values) => {
@@ -117,12 +118,12 @@ export const Checkout = () => {
                             <ErrorMessage name="telefono" component="div" className="text-danger" />
                         </div>
                         <div className="form-group">
-                            <Field type="email" onChange={handleChange} value={values.correo} name="correo" placeholder="Correo" className="form-control" />
-                            <ErrorMessage name="correo" component="div" className="text-danger" />
+                            <Field type="email" onChange={handleChange} value={values.email} name="email" placeholder="email" className="form-control" />
+                            <ErrorMessage name="email" component="div" className="text-danger" />
                         </div>
                         <div className="form-group">
-                            <Field type="email" onChange={handleChange} value={values.repetirCorreo} name="repetirCorreo" placeholder="Repetir correo" className="form-control" />
-                            <ErrorMessage name="repetirCorreo" component="div" className="text-danger" />
+                            <Field type="email" onChange={handleChange} value={values.repetirEmail} name="repetirEmail" placeholder="Repetir Email" className="form-control" />
+                            <ErrorMessage name="repetirEmail" component="div" className="text-danger" />
                         </div>
                         <button className="btn btn-primary" type="submit">Enviar</button>
                     </Form>
